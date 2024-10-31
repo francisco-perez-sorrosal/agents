65cbac454012e8aa560780e5c4dd7726e1c40b43
Francisco Perez-Sorrosal <fperezsorrosal@gmail.com>
2024-10-31 15:53:50 -0700

    Add filter named entities agent with crewai

--- berkeley-course-2024/hackathon/src/hackathon/graph_creation_step_2_triples_crewai.py
+++ berkeley-course-2024/hackathon/src/hackathon/graph_creation_step_2_triples_crewai.py
@@ -31,27 +31,80 @@ def read_file(filename:str = "2405.14831v1.pdf"):
     return pickle.load(open(filename, "rb"))
 
 
+@tool
+def filter_named_entities(document_structure_with_entities_and_triples: List[dict]) -> List[dict]:
+    """Your function description here.
+
+    Args:
+        document_structure_with_entities_and_triples (List[dict]): A list of dictionaries containing 
+        the document structure with entities and triples.
+
+    Returns:
+        List[dict]: A list of the named entities mentioned in the document.
+    """
+    for chunk_info in document_structure_with_entities_and_triples:
+        named_entities = chunk_info["named_entities"]
+        logger.info(named_entities)
+        named_entities = [entity.lower() for entity in named_entities]
+        triples = chunk_info["triples"]
+        logger.info(f"Initial Named Entities ({len(named_entities)}): {named_entities}")
+        named_entities: set = set(named_entities)
+        logger.info(f"Initial Named Entities after dedup ({len(named_entities)}): {named_entities}")
+        wrong_triples = 0
+        for triple in triples:
+            if len(triple) != 3:
+                wrong_triples += 1
+                continue
+            named_entities.add(triple[0].lower())
+            named_entities.add(triple[2].lower())
+        logger.info(f"Final Named Entities ({len(named_entities)}): {named_entities}")
+        chunk_info["named_entities"] = list(named_entities)
+    return document_structure_with_entities_and_triples[:1] # TODO Remove this filter!!!! Just for testing
+
+
 entity_master = Persona.from_yaml_file("Personas/EntityMasterCrewAI.yaml")
-code_generator_role: Role = entity_master.get_role("file_reader")
+file_reader_role: Role = entity_master.get_role("file_reader")
+entity_filter_role: Role = entity_master.get_role("entity_filter")
 
+class DocumentStructure(BaseModel):
+    id: int
+    text: str
+    named_entities: List[str]
+    triples: List[List[str]]
+    
+class DocumentStructures(BaseModel):    
+    document_structures: List[DocumentStructure]
 
-file_reader_agent: Agent = code_generator_role.to_crewai_agent(verbose=True,
+file_reader_agent: Agent = file_reader_role.to_crewai_agent(verbose=True,
                                                                allow_delegation=False,
                                                                allow_code_execution=False,
-                                                               tools=[read_file])
-file_reader_task = code_generator_role.get_crew_ai_task("read_file", file_reader_agent, tools=[read_file])
+                                                               tools=[read_file],)
+file_reader_task = file_reader_role.get_crew_ai_task("read_file", 
+                                                     file_reader_agent, 
+                                                     tools=[read_file], #)
+                                                     output_json=DocumentStructures,)
+
+entity_filter_agent: Agent = entity_filter_role.to_crewai_agent(verbose=True,
+                                                              allow_delegation=False,
+                                                              allow_code_execution=False,
+                                                              tools=[filter_named_entities])
+filter_entities_task = entity_filter_role.get_crew_ai_task("filter_entities", 
+                                                         entity_filter_agent, 
+                                                         tools=[filter_named_entities], #)
+                                                         output_json=DocumentStructures,)
 
 crew = Crew(
-    agents=[file_reader_agent], 
-    tasks=[file_reader_task],
-    # process=Process.sequential, # Sequential task execution
+    agents=[file_reader_agent, entity_filter_agent], 
+    tasks=[file_reader_task, filter_entities_task],
+    process=Process.sequential,  # Sequential task execution
 )
 
 # Kick off the process with the Python problem as input
-result = crew.kickoff(inputs={'filename': document_structure_file,},)
+crew_inputs = {'filename': document_structure_file,}
+result: CrewOutput = crew.kickoff(inputs=crew_inputs,)
 
 logger.info("---------")
 
-file_content = json.loads(json.dumps(result.raw))
+file_content = json.loads(json.dumps(result.json))
 logger.info(file_content)
 logger.info(type(file_content))
