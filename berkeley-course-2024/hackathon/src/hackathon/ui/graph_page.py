09a71d296ec9284ce7c9bc83c4f6b2d2bbaa6480
Francisco Perez-Sorrosal <fperezsorrosal@gmail.com>
2024-11-11 23:27:49 -0800

    Add Neo4j graph representation

--- berkeley-course-2024/hackathon/src/hackathon/ui/graph_page.py
+++ berkeley-course-2024/hackathon/src/hackathon/ui/graph_page.py
@@ -1,5 +1,18 @@
+import os
+
+import neo4j
+
+from pyvis.network import Network
 from shiny.express import ui, module, render
 from shiny import reactive
+
+from llm_foundation import logger
+from hackathon.graph_neo4j import Neo4jClientFactory
+
+# WWW directory definition for static assets
+DIR = os.path.dirname(os.path.abspath(__file__))
+WWW = os.path.join(DIR, "www")
+NEO4J_GRAPH_PYVIS_OUTPUT_ID = "neo4j_graph"
 
 @module
 def graph_page(input, output, session, sidebar_text):
@@ -15,3 +28,47 @@ def graph_page(input, output, session, sidebar_text):
     @render.code
     def out():
         return f"Sidebar text says {str(text.get())}"
+    
+    with ui.card(full_screen=True):
+        
+        @render.ui()
+        def graph():
+            neo4j_factory = Neo4jClientFactory()
+            graph_driver = neo4j_factory.neo4j_client()
+            
+            query_graph = graph_driver.execute_query("MATCH (n) OPTIONAL MATCH (n)-[r]->() RETURN n, r LIMIT 100",
+                                                name="Neurogen Graph",
+                                                result_transformer_=neo4j.Result.graph,)
+            visual_graph = Network()
+            
+            nodes_text_properties = {  # property shown as text per node
+                "Entity": "name",
+                "Episodic": "name",
+            }
+            
+            for node in query_graph.nodes:
+                node_label = list(node.labels)[0]
+                node_text = node[nodes_text_properties[node_label]]
+                visual_graph.add_node(node.element_id, node_text, group=node_label)
+            
+            for relationship in query_graph.relationships:
+                logger.info(f"Relationship: {relationship}")
+                visual_graph.add_edge(
+                    relationship.start_node.element_id,
+                    relationship.end_node.element_id,
+                    title=relationship.type
+                )
+            
+            # Visualize the graph as an iframe: https://github.com/posit-dev/py-shinywidgets/issues/63
+            visual_graph.generate_html(local=False)
+            file = os.path.join(WWW, NEO4J_GRAPH_PYVIS_OUTPUT_ID + ".html")
+            with open(file, "w") as f:
+                f.write(visual_graph.html)
+                
+            return ui.tags.iframe(
+                src=f"{NEO4J_GRAPH_PYVIS_OUTPUT_ID}.html",
+                style="height:600px;width:100%;",
+                scrolling="yes",
+                seamless="seamless",
+                frameBorder="3",
+            )
--- berkeley-course-2024/hackathon/src/hackathon/ui/main_app.py
+++ berkeley-course-2024/hackathon/src/hackathon/ui/main_app.py
@@ -1,10 +1,16 @@
 import os
-from shiny.express import input, ui
+from shiny.express import input, ui, app_opts
 from .chat_page import chat_page
 from .graph_page import graph_page
 from langtrace_python_sdk import langtrace  # Must precede any llm module imports
 
 from llm_foundation import logger
+
+
+# WWW directory definition for static assets
+DIR = os.path.dirname(os.path.abspath(__file__))
+WWW = os.path.join(DIR, "www")
+
 
 # We need to load the .env file in a function, otherwise there's an weird Shiny-related error
 def load_dotenv():
@@ -19,6 +25,7 @@ if bool(os.environ.get("OBSERVE_AGENTS")):
 logger.info("Starting main app...")
 
 # Configure Shiny page
+app_opts(static_assets=WWW)
 ui.page_opts(
     title="Neurogen",
     fillable=True,
